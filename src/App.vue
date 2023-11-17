0077cc70c11b2f3008037cdf0924f1c3ce53c8c4
Andres Legarreta <10548@ulsachihuahua.edu.mx>
2023-11-16 18:57:32 -0600

    agregando cambios incluyendo vue router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    <div id="app">
+      <b-container>
+        <b-row>
+          <b-col></b-col>
+          <b-col cols="6">
+            <b-card title="Register">
+            <b-form @submit="register">
+  
+                <br>
+              <b-form-group label="Username">
+                <b-form-input v-model="username" placeholder="Enter your username" required></b-form-input>
+              </b-form-group>
+              <br>
+              <b-form-group label="Password">
+                <b-form-input v-model="pass" type="password" placeholder="Enter your password" required></b-form-input>
+              </b-form-group>
+              <br>
+           
+  
+  
+              <b-button type="submit" variant="primary">Register</b-button>
+            </b-form>
+            <br>
+          </b-card>
+  
+          </b-col>
+          <b-col></b-col>
+  
+        </b-row>
+      </b-container>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name:'App',
+    components: {
+  
+    },
+    data(){
+      return{
+        username: "",
+        pass: "",
+       
+      }
+    },
+    methods:{
+      async register(event){
+        event.preventDefault();
+        const requestBody = {
+          us: this.username,
+          pass: this.pass,
+          
+        }
+  
+        const serveUrl = "https://tasty-pig-flip-flops.cyclic.app/";
+  
+        const response = await axios.post(
+          `${serveUrl}users/registrar`,
+          requestBody
+        );
+        console.log(response)
+      }
+    }
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
